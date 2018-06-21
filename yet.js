de297143064e4a7e51ddf9c3f1ec7aeef39d5f9f
AyeEmNutTeeBee <teebee10@hotmail.com>
2018-06-21 11:50:09 -0600

    Bullet

---
+++ yet.js
@@ -0,0 +1,13 @@
+// DOM -> Document Object Model
+
+const user = {
+    name: "Twinkle",
+    email: "dirtEsGud@moomoo.com",
+    favorites: [
+        'Dirt',
+        'Dead bushes'
+    ]
+}
+
+user.email
+user.favorites[1]
